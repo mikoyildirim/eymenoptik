9f66663ddf818aa8f0aa41c0e9ece1cc0ba747cd
mikoyildirim <yildirimmikail58@gmail.com>
2026-05-20 15:48:46 +0300

    aaa

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,19 +3,25 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Session\TokenMismatchException;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-       
-    $middleware->alias([
-    'admin' => \App\Http\Middleware\IsAdmin::class,
-]);
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\IsAdmin::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create();
+        $exceptions->render(function (TokenMismatchException $e, Request $request) {
+            return redirect()
+                ->route('login')
+                ->with('error', 'Oturum süreniz doldu. Lütfen tekrar giriş yapın.');
+        });
+    })
+    ->create();
